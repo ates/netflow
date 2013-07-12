d45500de1e87d432a8372a5569770cb404b4ed27
Artem Teslenko <ates@ipv6.dp.ua>
2013-07-13 01:32:59 +0300

    Fix formatting

--- src/netflow_v5_codec.erl
+++ src/netflow_v5_codec.erl
@@ -1,14 +1,11 @@
 -module(netflow_v5_codec).
 
+%% API
 -export([decode/1, encode/1]).
 
 -include("netflow_v5.hrl").
 
-%%
-%% API
-%%
-
-%% @doc Decodes the binary Netflow packet.
+%% @doc Decodes the binary NetFlow packet.
 -spec decode(binary()) -> {ok, {#nfh_v5{}, #nfrec_v5{}}} | {error, term()}.
 decode(Bin) ->
     try
@@ -28,45 +25,47 @@ encode(Record) ->
             {error, Reason}
     end.
 
-%%
 %% Internal functions
-%%
 
 decode_packet(<<?NF_V5_HEADER_FORMAT, Rest/binary>>) ->
-    Header = #nfh_v5{version = Version,
-                     count = Count,
-                     sys_uptime = SysUptime,
-                     unix_secs = UnixSecs,
-                     unix_nsecs = UnixNsecs,
-                     flow_seq = FlowSequence,
-                     engine_type = EngineType,
-                     engine_id = EngineID,
-                     sampling_interval = SamplingInterval},
+    Header = #nfh_v5{
+        version = Version,
+        count = Count,
+        sys_uptime = SysUptime,
+        unix_secs = UnixSecs,
+        unix_nsecs = UnixNsecs,
+        flow_seq = FlowSequence,
+        engine_type = EngineType,
+        engine_id = EngineID,
+        sampling_interval = SamplingInterval
+    },
     {ok, {Header, decode_records(Rest, [])}}.
 
 decode_records(<<>>, List) ->
     lists:reverse(List);
 decode_records(<<?NF_V5_RECORD_FORMAT, Rest/binary>>, List) ->
-    Record = #nfrec_v5{src_addr = SrcAddr,
-                       dst_addr = DstAddr,
-                       next_hop = NextHop,
-                       input = Input,
-                       output = Output,
-                       d_pkts = Pkts,
-                       d_octets = Octets,
-                       first = First,
-                       last = Last,
-                       src_port = SrcPort,
-                       dst_port = DstPort,
-                       pad1 = Pad1,
-                       tcp_flags = TcpFlags,
-                       prot = Prot,
-                       tos = Tos,
-                       src_as = SrcAs,
-                       dst_as = DstAs,
-                       src_mask = SrcMask,
-                       dst_mask = DstMask,
-                       pad2 = Pad2},
+    Record = #nfrec_v5{
+        src_addr = SrcAddr,
+        dst_addr = DstAddr,
+        next_hop = NextHop,
+        input = Input,
+        output = Output,
+        d_pkts = Pkts,
+        d_octets = Octets,
+        first = First,
+        last = Last,
+        src_port = SrcPort,
+        dst_port = DstPort,
+        pad1 = Pad1,
+        tcp_flags = TcpFlags,
+        prot = Prot,
+        tos = Tos,
+        src_as = SrcAs,
+        dst_as = DstAs,
+        src_mask = SrcMask,
+        dst_mask = DstMask,
+        pad2 = Pad2
+    },
     decode_records(Rest, [Record | List]).
 
 encode_packet({Header, Records}) ->
