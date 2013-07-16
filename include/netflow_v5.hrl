723b8314f7a8e210514b7bbbf5adf30b066b23cc
Volodymyr Udalych <vovar@ipv6.dp.ua>
2013-07-16 22:56:13 +0300

    Troubleshooting

--- include/netflow_v5.hrl
+++ include/netflow_v5.hrl
@@ -1,18 +1,17 @@
 %% NetFlow header Version 5
 -define(NF_V5_HEADER_FORMAT,
-    Version:16,
-    Count:16,
-    SysUptime:32,
-    UnixSecs:32,
-    UnixNsecs:32,
-    FlowSequence:32,
-    EngineType:8,
-    EngineID:8,
-    SamplingInterval:16
-).
+        Version:16,
+        Count:16,
+        SysUptime:32,
+        UnixSecs:32,
+        UnixNsecs:32,
+        FlowSequence:32,
+        EngineType:8,
+        EngineID:8,
+        SamplingInterval:16
+       ).
 
 -record(nfh_v5, {
-<<<<<<< HEAD
           version :: 5,
           count :: 1..30,
           sys_uptime :: non_neg_integer(),
@@ -22,44 +21,31 @@
           engine_type :: 0..255,
           engine_id :: 0..255,
           sampling_interval :: non_neg_integer()}).
-=======
-        version,
-        count,
-        sys_uptime,
-        unix_secs,
-        unix_nsecs,
-        flow_seq,
-        engine_type,
-        engine_id,
-        sampling_interval
-}).
->>>>>>> upstream/master
 
 %% NetFlow record Version 5 (V5)
 -define(NF_V5_RECORD_FORMAT,
-    SrcAddr:32,
-    DstAddr:32,
-    NextHop:32,
-    Input:16,
-    Output:16,
-    Pkts:32,
-    Octets:32,
-    First:32,
-    Last:32,
-    SrcPort:16,
-    DstPort:16,
-    Pad1:8,
-    TcpFlags:8,
-    Prot:8,
-    Tos:8,
-    SrcAs:16,
-    DstAs:16,
-    SrcMask:8,
-    DstMask:8,
-    Pad2:16
-).
+        SrcAddr:32,
+        DstAddr:32,
+        NextHop:32,
+        Input:16,
+        Output:16,
+        Pkts:32,
+        Octets:32,
+        First:32,
+        Last:32,
+        SrcPort:16,
+        DstPort:16,
+        Pad1:8,
+        TcpFlags:8,
+        Prot:8,
+        Tos:8,
+        SrcAs:16,
+        DstAs:16,
+        SrcMask:8,
+        DstMask:8,
+        Pad2:16
+       ).
 
-<<<<<<< HEAD
 -record(nfrec_v5, {src_addr :: non_neg_integer(),
                    dst_addr :: non_neg_integer(),
                    next_hop :: non_neg_integer(),
@@ -80,27 +66,3 @@
                    src_mask :: pos_integer(),
                    dst_mask :: pos_integer(),
                    pad2 :: 0}).
-=======
--record(nfrec_v5, {
-        src_addr,
-        dst_addr,
-        next_hop,
-        input,
-        output,
-        d_pkts,
-        d_octets,
-        first,
-        last,
-        src_port,
-        dst_port,
-        pad1,
-        tcp_flags,
-        prot,
-        tos,
-        src_as,
-        dst_as,
-        src_mask,
-        dst_mask,
-        pad2
-}).
->>>>>>> upstream/master
