4b4c82e05edcfa9ab786161e844f78f21bf53117
Artem Teslenko <ates@ipv6.dp.ua>
2017-02-14 19:09:50 +0200

    Refactor netflow v5 codec

--- src/netflow_v5_codec.erl
+++ src/netflow_v5_codec.erl
@@ -1,9 +1,45 @@
 -module(netflow_v5_codec).
 
 %% API
--export([decode/1, encode/1]).
+-export([decode/1]).
+-export([encode/1]).
 
 -include("netflow_v5.hrl").
+
+-define(NF_V5_HEADER_FORMAT,
+    Version:16,
+    Count:16,
+    SysUptime:32,
+    UnixSecs:32,
+    UnixNsecs:32,
+    FlowSequence:32,
+    EngineType:8,
+    EngineID:8,
+    SamplingInterval:16
+).
+
+-define(NF_V5_RECORD_FORMAT,
+    SrcAddr:32,
+    DstAddr:32,
+    NextHop:32,
+    Input:16,
+    Output:16,
+    Pkts:32,
+    Octets:32,
+    First:32,
+    Last:32,
+    SrcPort:16,
+    DstPort:16,
+    Pad1:8,
+    TcpFlags:8,
+    Prot:8,
+    Tos:8,
+    SrcAs:16,
+    DstAs:16,
+    SrcMask:8,
+    DstMask:8,
+    Pad2:16
+).
 
 %% @doc Decodes the binary NetFlow packet.
 -spec decode(binary()) -> {ok, {#nfh_v5{}, #nfrec_v5{}}} | {error, term()}.
@@ -29,55 +65,81 @@ encode(Record) ->
 
 decode_packet(<<?NF_V5_HEADER_FORMAT, Rest/binary>>) ->
     Header = #nfh_v5{
-        version = Version,
-        count = Count,
-        sys_uptime = SysUptime,
-        unix_secs = UnixSecs,
-        unix_nsecs = UnixNsecs,
-        flow_seq = FlowSequence,
-        engine_type = EngineType,
-        engine_id = EngineID,
+        version           = Version,
+        count             = Count,
+        sys_uptime        = SysUptime,
+        unix_secs         = UnixSecs,
+        unix_nsecs        = UnixNsecs,
+        flow_seq 		  = FlowSequence,
+        engine_type       = EngineType,
+        engine_id 		  = EngineID,
         sampling_interval = SamplingInterval
     },
     {ok, {Header, decode_records(Rest, [])}}.
 
-decode_records(<<>>, List) ->
-    lists:reverse(List);
 decode_records(<<?NF_V5_RECORD_FORMAT, Rest/binary>>, List) ->
     Record = #nfrec_v5{
-        src_addr = SrcAddr,
-        dst_addr = DstAddr,
-        next_hop = NextHop,
-        input = Input,
-        output = Output,
-        d_pkts = Pkts,
-        d_octets = Octets,
-        first = First,
-        last = Last,
-        src_port = SrcPort,
-        dst_port = DstPort,
-        pad1 = Pad1,
+        src_addr  = SrcAddr,
+        dst_addr  = DstAddr,
+        next_hop  = NextHop,
+        input     = Input,
+        output    = Output,
+        d_pkts    = Pkts,
+        d_octets  = Octets,
+        first     = First,
+        last      = Last,
+        src_port  = SrcPort,
+        dst_port  = DstPort,
+        pad1      = Pad1,
         tcp_flags = TcpFlags,
-        prot = Prot,
-        tos = Tos,
-        src_as = SrcAs,
-        dst_as = DstAs,
-        src_mask = SrcMask,
-        dst_mask = DstMask,
-        pad2 = Pad2
+        prot      = Prot,
+        tos       = Tos,
+        src_as    = SrcAs,
+        dst_as    = DstAs,
+        src_mask  = SrcMask,
+        dst_mask  = DstMask,
+        pad2      = Pad2
     },
-    decode_records(Rest, [Record | List]).
+    decode_records(Rest, [Record | List]);
+decode_records(<<>>, List) ->
+	lists:reverse(List).
 
 encode_packet({Header, Records}) ->
-    {nfh_v5, Version, Count, SysUptime, 
-        UnixSecs, UnixNsecs, FlowSequence, 
-        EngineType, EngineID, SamplingInterval} = Header,
-    Rec = list_to_binary([encode_record(R) || R <- Records]),
+    #nfh_v5{
+        version           = Version,
+        count             = Count,
+        sys_uptime        = SysUptime,
+        unix_secs         = UnixSecs,
+        unix_nsecs        = UnixNsecs,
+        flow_seq          = FlowSequence,
+        engine_type       = EngineType,
+        engine_id         = EngineID,
+        sampling_interval = SamplingInterval
+    } = Header,
+    Rec = << <<(encode_record(R))/binary>> || R <- Records>>,
     {ok, <<?NF_V5_HEADER_FORMAT, Rec/binary>>}.
 
 encode_record(Record) ->
-    {nfrec_v5, SrcAddr, DstAddr, NextHop, 
-        Input, Output, Pkts, Octets, First, 
-        Last, SrcPort, DstPort, Pad1, TcpFlags, 
-        Prot, Tos, SrcAs, DstAs, SrcMask, DstMask, Pad2} = Record,
+    #nfrec_v5{
+        src_addr  = SrcAddr,
+        dst_addr  = DstAddr,
+        next_hop  = NextHop,
+        input     = Input,
+        output    = Output,
+        d_pkts    = Pkts,
+        d_octets  = Octets,
+        first 	  = First,
+        last 	  = Last,
+        src_port  = SrcPort,
+        dst_port  = DstPort,
+        pad1	  = Pad1,
+        tcp_flags = TcpFlags,
+        prot      = Prot,
+        tos       = Tos,
+        src_as    = SrcAs,
+        dst_as    = DstAs,
+        src_mask  = SrcMask,
+        dst_mask  = DstMask,
+        pad2      = Pad2
+	} = Record,
     <<?NF_V5_RECORD_FORMAT>>.
